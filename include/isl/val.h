8ca6a785751b849a16573758df66b50a92550057
Armin Größlinger <armin.groesslinger@uni-passau.de>
2015-01-31 02:04:46 +0100

    add __isl_export annotations to print methods
    This restores the ability to print isl objects in
    language bindings.
    
    Signed-off-by: Armin Größlinger <armin.groesslinger@uni-passau.de>
    Signed-off-by: Andreas Simbuerger <simbuerg@fim.uni-passau.de>

--- include/isl/val.h
+++ include/isl/val.h
@@ -105,6 +105,7 @@ isl_bool isl_val_is_divisible_by(__isl_keep isl_val *v1,
 
 __isl_constructor
 __isl_give isl_val *isl_val_read_from_str(isl_ctx *ctx, const char *str);
+__isl_export
 __isl_give isl_printer *isl_printer_print_val(__isl_take isl_printer *p,
 	__isl_keep isl_val *v);
 void isl_val_dump(__isl_keep isl_val *v);
