f665d0c17aaeec261f0e6d6c2243750775035863
Armin Größlinger <armin.groesslinger@uni-passau.de>
2015-01-31 02:04:58 +0100

    band.h: annotate isl_band as __isl_inplace
    isl_bands only exist while the associated isl_schedule exists.
    
    Signed-off-by: Armin Größlinger <armin.groesslinger@uni-passau.de>
    Signed-off-by: Andreas Simbuerger <simbuerg@fim.uni-passau.de>

--- include/isl/band.h
+++ include/isl/band.h
@@ -10,7 +10,7 @@
 extern "C" {
 #endif
 
-struct isl_band;
+struct __isl_export __isl_inplace isl_band;
 typedef struct isl_band isl_band;
 
 ISL_DECLARE_LIST(band)
@@ -24,10 +24,13 @@ int isl_band_has_children(__isl_keep isl_band *band);
 __isl_give isl_band_list *isl_band_get_children(
 	__isl_keep isl_band *band);
 
+__isl_export
 __isl_give isl_union_map *isl_band_get_prefix_schedule(
 	__isl_keep isl_band *band);
+__isl_export
 __isl_give isl_union_map *isl_band_get_partial_schedule(
 	__isl_keep isl_band *band);
+__isl_export
 __isl_give isl_union_map *isl_band_get_suffix_schedule(
 	__isl_keep isl_band *band);
 
@@ -36,12 +39,15 @@ int isl_options_get_tile_scale_tile_loops(isl_ctx *ctx);
 isl_stat isl_options_set_tile_shift_point_loops(isl_ctx *ctx, int val);
 int isl_options_get_tile_shift_point_loops(isl_ctx *ctx);
 
+__isl_export
 int isl_band_tile(__isl_keep isl_band *band, __isl_take isl_vec *sizes);
+__isl_export
 int isl_band_split(__isl_keep isl_band *band, int pos);
 
 int isl_band_n_member(__isl_keep isl_band *band);
 int isl_band_member_is_coincident(__isl_keep isl_band *band, int pos);
 
+__isl_export
 int isl_band_list_foreach_band(__isl_keep isl_band_list *list,
 	int (*fn)(__isl_keep isl_band *band, void *user), void *user);
 
