79386a84a3878c20a906844508ddddb632eee0db
Andreas Simbuerger <simbuerg@fim.uni-passau.de>
2015-08-06 16:23:20 +0200

    add schedule_node to all.h

--- interface/all.h
+++ interface/all.h
@@ -4,6 +4,7 @@
 #include <isl/set.h>
 #include <isl/map.h>
 #include <isl/schedule.h>
+#include <isl/schedule_node.h>
 #include <isl/union_set.h>
 #include <isl/union_map.h>
 #include <isl/polynomial.h>
